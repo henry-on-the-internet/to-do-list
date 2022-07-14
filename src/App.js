7e29a4c09b6b66f170c1ca330c3821299f38e104
henry-on-the-internet <narrator0120@gmail.com>
2022-07-14 15:25:38 +1000

    add variable

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = "React";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, {subject}!
         </p>
         <a
           className="App-link"
