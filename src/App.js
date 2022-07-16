d9358c5dde1090cbedb656a227877cd5796b15f0
henry-on-the-internet <narrator0120@gmail.com>
2022-07-16 16:26:44 +1000

    enable filter buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ import { nanoid } from 'nanoid';
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
+  const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+  };
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name} 
+      isPressed={name === filter}
+      setFilter={setFilter}  
+    />
+  ));
   function addTask(name){
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
@@ -42,7 +57,9 @@ function App(props) {
     setTasks(updatedTasks);
   }
   
-  const taskList = tasks.map(task => 
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => 
     <Todo 
       id={task.id} 
       name={task.name} 
@@ -60,7 +77,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>    
       <div className="filters btn-group stack-exception">
-        <FilterButton/>
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
