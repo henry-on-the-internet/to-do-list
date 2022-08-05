11a1d403263b6faa66bdb9c25fbcd5b80675d754
henry-on-the-internet <narrator0120@gmail.com>
2022-08-05 23:24:54 +1000

    modify default todo items

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-0", name: "Learn React", completed: true },
+  { id: "todo-1", name: "Testing", completed: false },
+  { id: "todo-2", name: "Deployment", completed: false }
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
